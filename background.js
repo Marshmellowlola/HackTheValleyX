5f9630cc8a9a21887b0db024db335c5f3851faba
Marshmellowlola <marshmellowlola@gmail.com>
2025-10-05 03:33:18 -0400

    Gemini intergrated

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+import { GoogleGenAI } from "./genai.js";
+import { key } from "./API_KEYS.js";
+
 let time = Date.now();
 const interval = 3000; // 5 secs
 
@@ -23,6 +26,17 @@ function stopTime(timer){
 	clearInterval(timer)
 }
 
+const block = async(question) => {
+	// generate question using ai
+	// add question onto site
+	document.body.style.display = "none"
+	const styleSheet = document.styleSheets[0]
+	console.log('html::before { content: "'+ question +'"; } ')
+	styleSheet.insertRule('html::before { content:"'+ question +'"; } ', styleSheet.cssRules.length)
+
+	startTime()
+}
+
 async function trackTime(){
 	if ((Date.now() - time) >= interval) { //over the interval time
 		time = Date.now()
@@ -31,11 +45,26 @@ async function trackTime(){
 		const tab = await getTab()
 		console.log(await getTab())
 		//script that runs the blocking script
+
+		//const { GoogleGenAI } = await import ("https://cdn.jsdelivr.net/npm/@google/genai/+esm");
+		const ai = new GoogleGenAI({apiKey: key});
+		const response = await ai.models.generateContent({
+			model: "gemini-2.5-flash",
+			contents: "Explain how AI works in a few words", // ask a question about...
+			config: {
+			  systemInstruction: "You are a cat. Your name is Neko.", // change
+			},
+		});
+		console.log(response.text);
+
+		//generative stuff
 		chrome.scripting.executeScript({
 			target: {"tabId":tab.id},
-			files: ["block.js"],
+			function: block,
+			args: [response.text]
+			//files: ["block.js"],
 		})
-		.then(() => startTime())
+
 		// keep in while loop
 
 		//start time again
--- block.js
+++ block.js
@@ -1,9 +1,11 @@
 document.body.style.display = "none"
 
+//add question onto site
 const styleSheet = document.styleSheets[0]
 styleSheet.insertRule('html::before { content:"!!"; } ', styleSheet.cssRules.length)
 
-while () {
+
+// while () {
 // ask ai for question -> display
 // answer box, give to ai, recieve answer
-}
+// }
