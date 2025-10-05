4d6fafc611661f3674389bfb765d8a785b767340
Marshmellowlola <marshmellowlola@gmail.com>
2025-10-05 01:32:26 -0400

    Text on site

--- block.js
+++ block.js
@@ -1,5 +1,9 @@
+document.body.style.display = "none"
 
-console.log("it works")
-const popup = document.createElement("div")
-popup.innerHTML = "<p>testing</p>"
-document.body.style.display = false
+const styleSheet = document.styleSheets[0]
+styleSheet.insertRule('html::before { content:"!!"; } ', styleSheet.cssRules.length)
+
+while () {
+// ask ai for question -> display
+// answer box, give to ai, recieve answer
+}
